5a439dc8e1c5550e7430ddbc24e8f3fe50f8954b
stepin105158 <68631577+stepin105158@users.noreply.github.com>
2020-10-05 08:20:19 +0530

    Update test_evm.c

--- 03_Implementation/test/test_evm.c
+++ 03_Implementation/test/test_evm.c
@@ -2,27 +2,18 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
 
-
 #define PROJECT_NAME    "EVM"
 
 
 void test_boothid(void);
-
-
 void test_leadingCandidate(void);
-
 
 int main() {
 
   if (CUE_SUCCESS != CU_initialize_registry())
     return CU_get_error();
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
-
-
-
-
   CU_add_test(suite, "boothid", test_boothid);
- 
   CU_add_test(suite, "leadingCandidate", test_leadingCandidate);
 
 
