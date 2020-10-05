bb2a1977163f15febdb311042ec3cea38341c40f
stepin105158 <68631577+stepin105158@users.noreply.github.com>
2020-10-05 19:30:21 +0530

    Update evm_operations.h

--- 03_Implementation/inc/evm_operations.h
+++ 03_Implementation/inc/evm_operations.h
@@ -16,12 +16,12 @@
 
 //int votesCount1=0, votesCount2=0, votesCount3=0, votesCount4=0, spoiledtvotes=0;
 int boothid(int id);
-void menu();
+//void menu();
 int adLogin(char uname[10],char pword[10]);
-void castVote();
-int CheckVoterId(FILE *fptr, const char *word, int *line, int *col);
-void votesCount();
-int leadingCandidate(int x,int y);
+//void castVote();
+//int CheckVoterId(FILE *fptr, const char *word, int *line, int *col);
+//void votesCount();
+//int leadingCandidate(int x,int y);
 
 
 #endif
